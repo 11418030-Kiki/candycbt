dad0386c78b8b7511265404bc2c89beb96c8a4ae
sutam21 <servergiveaway@gmail.com>
2020-04-23 19:24:04 +0700

    CandyCBT v 2.8 r2

---
+++ admin/materi/buat_materi.php
@@ -0,0 +1,45 @@
+<?php
+require("../../config/config.default.php");
+require("../../config/config.function.php");
+require("../../config/functions.crud.php");
+cek_session_guru();
+
+$id_mapel = $_POST['id_mapel'];
+$id_guru = $_SESSION['id_pengawas'];
+$materi = $_POST['materi'];
+$judul = $_POST['judul'];
+
+$ektensi = ['jpg', 'png'];
+if ($_FILES['file']['name'] <> '') {
+   $file = $_FILES['file']['name'];
+   $temp = $_FILES['file']['tmp_name'];
+   $ext = explode('.', $file);
+   $ext = end($ext);
+   if (in_array($ext, $ektensi)) {
+      $dest = '../../berkas/';
+      $path = $dest . $file;
+      $upload = move_uploaded_file($temp, $path);
+      if ($upload) {
+         $data = array(
+            'id_mapel' => $id_mapel,
+            'idguru' => $id_guru,
+            'judul' => $judul,
+            'materi' => $materi,
+            'file' => $file
+         );
+         insert($koneksi, 'materi', $data);
+         echo "ok";
+      } else {
+         echo "gagal";
+      }
+   }
+} else {
+   $data = array(
+      'id_mapel' => $id_mapel,
+      'idguru' => $id_guru,
+      'judul' => $judul,
+      'materi' => $materi
+   );
+   insert($koneksi, 'materi', $data);
+   echo "ok";
+}
